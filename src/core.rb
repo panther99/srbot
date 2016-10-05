71d9392a6336b32ff0365e9f142e26749a5185a7
panther99 <nikola.stojakovic@hotmail.com>
2016-10-05 19:22:30 +0200

    Fixed error

--- src/core.rb
+++ src/core.rb
@@ -5,7 +5,7 @@ def on(hash)
 	if hash.is_a? Hash
 		puts category_standard(hash.keys[0], hash.values[0])
 	else
-		raise "hash parameter for get function is not of Hash type"
+		raise "hash parameter for on function is not of Hash type"
 	end
 end
 
