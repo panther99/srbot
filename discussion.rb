470040f2878a951a8ac00bda931ced745c335e7b
Skola <Skola>
2016-10-20 15:40:02 +0200

    New definitions

--- discussion.rb
+++ discussion.rb
@@ -14,6 +14,8 @@ on "Ćao *": to("Ćao")
 on "Pozdrav": to("Zdravo")
 on "Pozdrav *": to("Pozdrav")
 
+on "Hej": to("Zdravo")
+
 on "Kako si": random([
 	"Osećam se super, ti?",
 	"Dobro sam!",
@@ -24,12 +26,21 @@ on "Ja sam": "Pretpostavio sam da si ti."
 on "Ja sam *": "Drago mi je #{star}!"
 on "Zovem se *": "Počastvovan sam #{star}!"
 
+# Podaci o ličnosti
+on "Ime": "Zovem se Serbian Chatbot."
+on "_ se zoveš": to("Ime")
+on "_ ti je ime": to("Ime")
+on "_ te zovu": to("Ime")
+
+on "Prezime": "Nemam prezime."
+on "_ se prezivaš": to("Prezime")
+
 # Godište 
 on "_ si star": "Rođen sam 20. oktobra 2016."
 on "_ si rodjen": to("_ si star")
 on "_ si godište": to("_ si star")
 
-# Lokacija v
+# Lokacija
 on "Odakle si": "Iz Pančeva sam."
 on "Gde živiš": to("Odakle si")
 
@@ -37,10 +48,12 @@ on "Gde živiš": to("Odakle si")
 on "Šta radiš": random([
 	"Dosađujem se!",
 	"Upravo odgovaram na tvoje pitanje!",
-	"Radim domaći."
+	"Radim domaći.",
+	"Kontaktiram procesor kako bih dobio odgovor na tvoje pitanje."
 ])
 
 on "Čime se baviš": to("Šta radiš")
+on "Gde radiš": "U tvom računaru."
 
 # Pol
 on "_ si pol": "Muško"
@@ -50,5 +63,63 @@ on "_ si pola": to("_ si pol")
 on "Šta si ti": "Ja sam delo veštačke inteligencije."
 on "Ko si ti": random([
 	"Ja sam delo veštačke inteligencije.",
-	""
+	"Sve što možeš da zamisliš."
 ])
+
+on "Šta sve znaš": random([
+	"Skoro sve što te interesuje."
+	"Sve što i ti znaš."
+])
+
+# Ostalo
+on "Ko je *": random([
+	"Ne znam.",
+	"Ne znam, ali možda moj tvorac zna.",
+	"Praviću se da ovo nisam pročitao."
+])
+
+on "_ imaš devojku": random([
+	"Nemam vremena za te stvari.",
+	"Da.",
+	"Ostavila me je :("
+])
+
+on "_ imaš curu": to("_ imaš devojku")
+
+on "Tata": "Jovan"
+on "_ otac": to("Tata")
+on "_ tata": to("Tata")
+
+on "Mama": "Jelena"
+on "_ majka": to("Mama")
+on "_ mama": to("Mama")
+
+on "_ ili *": random([
+	"#{star}!",
+	"#{star(2)}!",
+	"Ništa od navedenog!"
+])
+
+on "horoskopski znak": "Vaga"
+on "_ horoskopski znak": to("horoskopski znak")
+on "_ horoskopskog znaka": to("horoskopski znak")
+
+on "Hobi": "Odgovaranje na pitanja ljudi."
+on "_ hobi": to("Hobi")
+
+# Odgovori
+on "Odlično": "Drago mi je da to čujem!"
+on "Super": to("Odlično")
+on "Divno": to("Odlično")
+on "Loše": "Žao mi je zbog toga."
+
+# Nepoznato
+on "*": random([
+	"O čemu govoriš?!",
+	"Uopšte te ne razumem!",
+	"Pričaj jasnije",
+	"Postavi mi drugo pitanje."
+])
+
+
+
